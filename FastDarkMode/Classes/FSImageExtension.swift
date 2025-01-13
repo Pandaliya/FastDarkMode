0b7c74e963f5e39fc6f3c5875f2d953aa4b989cb
zhangpan <zhangpan@cls.cn>
2025-01-13 18:22:28 +0800

    update

--- FastDarkMode/Classes/FSImageExtension.swift
+++ FastDarkMode/Classes/FSImageExtension.swift
@@ -10,7 +10,7 @@ import Foundation
 var ModeBundle: Bundle? = nil
 
 extension Bundle {
-    static var modeBundle: Bundle? {
+    public static var modeBundle: Bundle? {
         if ModeBundle == nil {
             var path = Bundle.main.url(forResource: "Frameworks", withExtension: nil)
             path = path?.appendingPathComponent("FastDarkMode")
@@ -24,5 +24,32 @@ extension Bundle {
             }
         }
         return ModeBundle
+    }
+}
+
+extension UIImage {
+    /// 使用不同bundle的图片（podfile使用use_frameworks）
+    /// - Parameter name: 图片名称
+    /// - Returns: 图片数据
+    public static func modeImage(bundle: Bundle?, name:String) -> UIImage? {
+        guard let smb = bundle else {
+            return nil
+        }
+        
+        let styleName = name + FSModeManager.shared.imageNameTile
+        
+        if #available(iOS 13.0, *) {
+            let image = UIImage(named: styleName, in: smb, with: nil)
+            if image != nil {
+                return image
+            }
+            return UIImage(named: name, in: smb, with: nil)
+        } else {
+            let img =  UIImage.init(named: styleName, in: smb, compatibleWith: nil)
+            if img != nil {
+                return img
+            }
+            return UIImage.init(named: name, in: smb, compatibleWith: nil)
+        }
     }
 }
